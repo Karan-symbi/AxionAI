9cc6bfc45b5f26274e9ec3a66532f3f06b01ccfc
Mr-Thop <karthikreddy0314@gmail.com>
2026-01-31 11:46:10 +0530

    Add feature Parse PDF

---
+++ Parser.py
@@ -0,0 +1,21 @@
+from langchain_community.document_loaders import PyPDFLoader
+
+class Parser():
+    def __init__(self):
+        pass
+    def parse(self,file_path):
+        loader = PyPDFLoader(file_path)
+        pages = loader.load_and_split()
+        no_of_pages = pages[0].metadata["total_pages"]
+        content = []
+        for i in range(no_of_pages):
+            content.append(pages[i].page_content)
+
+        return {
+            "content": content,
+            "no_of_pages": no_of_pages
+        }
+
+
+parser = Parser()
+print(parser.parse("D:\Download\Hackathons\AIBoomi\example.pdf"))
